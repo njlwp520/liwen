4c5ca248a60381cc4f381c46ebed51e1f464eb21
liwen <liwen@176.130.23.39>
2019-11-07 20:41:03 +0800

    sfdg

---
+++ test-bootstrap/src/main/java/com/liwen/TestBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.liwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBootstrapApplication.class, args);
+	}
+
+}
